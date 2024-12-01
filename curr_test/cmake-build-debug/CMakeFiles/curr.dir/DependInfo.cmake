e193833c715f68b8148b564da62791665813c158
WTY2002 <81131511+WTY2002@users.noreply.github.com>
2024-12-01 13:47:52 +0800

    commit：添加10种分布式数据安全协同计算

---
+++ curr_test/cmake-build-debug/CMakeFiles/curr.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/wty/xxxxx/tmp/tmp.lfmOM9qZiT/include/PHE.cpp" "/root/wty/xxxxx/tmp/tmp.lfmOM9qZiT/cmake-build-debug/CMakeFiles/curr.dir/include/PHE.cpp.o"
+  "/root/wty/xxxxx/tmp/tmp.lfmOM9qZiT/include/SHE.cpp" "/root/wty/xxxxx/tmp/tmp.lfmOM9qZiT/cmake-build-debug/CMakeFiles/curr.dir/include/SHE.cpp.o"
+  "/root/wty/xxxxx/tmp/tmp.lfmOM9qZiT/test/main.cpp" "/root/wty/xxxxx/tmp/tmp.lfmOM9qZiT/cmake-build-debug/CMakeFiles/curr.dir/test/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ curr_test/cmake-build-debug/CMakeFiles/curr.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/curr.dir/include/PHE.cpp.o"
+  "CMakeFiles/curr.dir/include/SHE.cpp.o"
+  "CMakeFiles/curr.dir/test/main.cpp.o"
+  "curr"
+  "curr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/curr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
